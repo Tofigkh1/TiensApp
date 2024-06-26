e6b8257fde676261da6c14c3d52cf3c15c9cef6e
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-06-26 18:10:01 +0400

    -tailwind css bug fix and all the pages router if created

--- Shared/Components/Client/Header/Header.tsx
+++ Shared/Components/Client/Header/Header.tsx
@@ -1,3 +1,26 @@
 import styless from './navbar.module.css'
+import Nav from '../Nav/Nav'
+import { useResize } from '../../../Hooks/useResize'
+import {useModalOpen} from "../../../Hooks/useModalOpen";
+import { useRouter } from 'next/router';
+import Logo from "../../Svg/Logo"
 
 
+export default function Header() {
+    let {isOpen, onOpen, onClose} = useModalOpen();
+    let {isMobile} = useResize();
+    let {push} = useRouter();
+
+    return (
+        <>
+        <section>
+            <div>
+                <button className=''></button>
+                <div className='cursor pointer' onClick={()=>push('/')}><Logo/></div>
+            </div>
+
+            <Nav/>
+        </section>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./shared/**/*.{js,ts,jsx,tsx,mdx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      grayText: "#828282",
+      grayText2: "#4F4F4F",
+      whiteLight1: "#F3F4F6",
+      mainRedLight: "rgba(214, 54, 38, 0.3)",
+      whiteLight3: "#BDBDBD",
+    },
+
+
+    extend: {},
+  },
+  plugins: [],
+}
+
