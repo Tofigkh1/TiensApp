bfa8d8718895083fb717976c7a8f25bc090f6b0b
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-06-26 15:38:02 +0400

    test3

--- Shared/Components/Client/Nav/Nav.tsx
+++ Shared/Components/Client/Nav/Nav.tsx
@@ -1,6 +1,11 @@
 import { useRouter } from 'next/router'
-
 import styless from './Nav.module.css'
+import {useResize} from "../../../Hooks/useResize";
+import { useEffect, useState } from 'react';
 
+export default function Nav() {
+    let {push, pathname} = useRouter();
+    let {isMobile} = useResize();
 
-const 
+    const [accessToken, setAccesToken] = useState(null);
+}
