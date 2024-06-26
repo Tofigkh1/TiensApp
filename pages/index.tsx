b33fd67d166d46978152af5e87b279d49cddfbb9
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-06-27 02:33:17 +0400

    -important components added -bugs fixed -navbar elements created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-
-
 import axios from "axios";
+import dynamic from "next/dynamic";
+import { NextPage } from "next";
 
-const Home: NextPage = () => {
 
+const MainLayout = dynamic(() => import("../Shared/Components/Layout/MainHeaderLayout"), { ssr: false });
 
+const Home: NextPage = (props) => {
+  console.log("MainLayout",MainLayout); 
   return (
-<>
-
-</>
+    <>
+      <MainLayout />
+    </>
   );
 };
 
