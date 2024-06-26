91717f5d0129f3e3da277d4e40fabf3286e21164
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-06-26 13:45:27 +0400

    restructuring

---
+++ Shared/Components/Client/Nav/Nav.tsx
@@ -0,0 +1,6 @@
+import { useRouter } from 'next/router'
+
+import styless from './Nav.module.css'
+
+
+const 
---
+++ Shared/Components/Layout/MainHeaderLayout.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head"
+import styless from "./MainHeaderLayout.module.css"
+
+
+
+
+const MainLayout = ({children}) => {
+    <div>
+        <Head>
+            <title>Tiens App</title>
+            <meta name="DoctorTibet" content="Doctor Tibet by Tiens App"/>
+            <link rel="icon" href="/favicon" />
+        </Head>
+        <div>
+            <div className="md:px-8">
+               
+
+            </div>
+        </div>
+    </div>
+}
