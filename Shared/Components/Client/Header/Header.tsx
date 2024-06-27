cc3a8fc1392cde4be8133abd5891156fcc328078
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-06-28 03:40:27 +0400

    -colorful and animated styles were given to the fonts in the header -logo added and corrected -router errors fixed -background image added

--- Shared/Components/Client/Header/Header.tsx
+++ Shared/Components/Client/Header/Header.tsx
@@ -4,6 +4,8 @@ import { useResize } from '../../../Hooks/useResize';
 import { useModalOpen } from '../../../Hooks/useModalOpen';
 import { useRouter } from 'next/router';
 import Logo from '../../Svg/Logo';
+import Bacground from '../../../../public/homeBacground.svg'
+import Image from 'next/image';
 
 export default function Header() {
     let { isOpen, onOpen, onClose } = useModalOpen();
@@ -11,18 +13,21 @@ export default function Header() {
     let { push } = useRouter();
 
     return (
-        <>
-            <section className={styles.header_box} >
-                <div>
+        <div className={styles.bgimage}>
+            <section className={`${styles.header_box} ${isOpen? styles.shadow : ''} `}>
+                <div className={`${styles.logo_box} flex gap-3 items-center`}>
                     <button className={styles.button}></button>
-
+                    <h1 className={styles.logotext}>Doctor Tibet</h1>
                     <div className={styles.cursor} onClick={() => push('/')}>
-                        <img className={styles.logo} src="/Logo.png" alt="Logo" />
+                        
+                        <img style={{width:'55px', height:'55px'}} className={styles.logo} src="/Logo.png" alt="Logo" />
                     </div>
                 </div>
 
                 <Nav />
+                
             </section>
-        </>
+          
+            </div>
     );
 }
