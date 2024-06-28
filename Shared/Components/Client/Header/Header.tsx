ec51dfa18b33ecaf173c1aff9407dbc866fe200f
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-06-29 01:41:51 +0400

    Auth component created and search input created and little ui bug fixed

--- Shared/Components/Client/Header/Header.tsx
+++ Shared/Components/Client/Header/Header.tsx
@@ -6,6 +6,8 @@ import { useRouter } from 'next/router';
 import Logo from '../../Svg/Logo';
 import Bacground from '../../../../public/homeBacground.svg'
 import Image from 'next/image';
+import Search from '../Search/Search';
+import Auth from '../Auth/Auth';
 
 export default function Header() {
     let { isOpen, onOpen, onClose } = useModalOpen();
@@ -17,16 +19,40 @@ export default function Header() {
             <section className={`${styles.header_box} ${isOpen? styles.shadow : ''} `}>
                 <div className={`${styles.logo_box} flex gap-3 items-center`}>
                     <button className={styles.button}></button>
-                    <h1 className={styles.logotext}>Doctor Tibet</h1>
+                    {/* <h1 className={styles.logotext}>Doctor Tibet</h1> */}
                     <div className={styles.cursor} onClick={() => push('/')}>
                         
-                        <img style={{width:'55px', height:'55px'}} className={styles.logo} src="/Logo.png" alt="Logo" />
+                        <img style={{width:'80px', height:'80px'}} className={styles.logo} src="/Logo.png" alt="Logo" />
                     </div>
                 </div>
 
+                <div>
                 <Nav />
-                
+                </div>
+               
+                <div className='flex flex-row gap-4 items-center'> 
+                  
+                    <div className={styles.mobile_hide}>
+                        <Auth/>
+                    </div>
+                </div>
             </section>
+
+            <div>
+            <div className={styles.headerText}>
+            <h1 >We can get your Drug</h1>
+            <h1>Prescriptions to You</h1>
+            </div>
+
+            <div className={styles.headerSmallText}>
+                <h1>We have all the drugs your doctor prescribed for your health</h1>
+                <h1>and what’s more, we can get it to you.</h1>
+            </div>
+            </div>
+           
+          
+           
+            <Search/>
           
             </div>
     );
