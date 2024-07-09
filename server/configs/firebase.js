aefceb2bb3903faed74ba9f3a67e937143181f0e
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-07-09 11:28:38 +0400

    feat: Enhance login and registration components
    - Modified .gitignore to exclude unnecessary files.
    - Updated Auth.tsx with improved authentication logic.
    - Added new Spinner component (index.tsx) for loading states.
    - Created RegisterForm component and its styles (index.tsx, registerForm.module.css).
    - Created LoginForm component and its styles (index.tsx, loginForm.module.css).
    - Added new input components for login and register forms with corresponding styles.
    - Updated package-lock.json and package.json with new dependencies.
    - Enhanced login-register page with new styles (index.tsx, loginRegister.module.css).
    - Added new public images (Login3.png, TiensAccount.png, TiensAccount2.png, TiensRegister.png, eye.svg).
    - Updated Firebase configuration files (firebase.js, firebaseAdmin.js) for better integration.
    
    This commit introduces significant improvements and new features for the client-side login and registration process, enhancing user experience and application maintainability.

---
+++ server/configs/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getStorage,ref } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAGh9AFmjbVUl-M0uoqr2cqnVcV5T1k5vM",
+    authDomain: "tiensapp-92bab.firebaseapp.com",
+    projectId: "tiensapp-92bab",
+    storageBucket: "tiensapp-92bab.appspot.com",
+    messagingSenderId: "736874346462",
+    appId: "1:736874346462:web:88c07df6a0e9978e790f4f"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  
+  export const storage = getStorage(app);
+  export  const storageRef = ref
