6620ca3975081887dcec0afde5b0a1bc8334d656
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-07-10 16:58:19 +0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,5 @@
-// import '../styles/globals.css'
-// import type { AppProps } from 'next/app'
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
-// export default MyApp
-
+import { wrapper } from '../Shared/Redux/Store/store'; 
+import { Provider } from 'react-redux'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { ChakraProvider } from "@chakra-ui/react";
@@ -16,11 +9,15 @@ import theme from "../Theme/index"; // Import your custom theme if you have one
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const {store} = wrapper.useWrappedStore(pageProps)
   return (
+    <Provider store={store}>
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+    </Provider>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
