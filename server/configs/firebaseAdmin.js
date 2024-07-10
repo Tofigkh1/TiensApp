f5f0dfd6aea8df0364aee3d1a068f19bad4019b2
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-07-10 21:03:13 +0400

    test3

--- server/configs/firebaseAdmin.js
+++ server/configs/firebaseAdmin.js
@@ -1,10 +1,10 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-    // const serviceAccount = require("./foody-3-firebase-adminsdk-8atpy-6897ff2d7c.json");
-    const serviceAccount = require("./tiensapp-92bab-firebase-adminsdk-gmgsg-bf619bb819");
+
+    const serviceAccount = require("./tiensapp-92bab-firebase-adminsdk-gmgsg-561f9f954a");
     
-    // const serviceAccount = require("./foody-5-firebase-adminsdk-pvv01-e301ba401b.json");
+
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
