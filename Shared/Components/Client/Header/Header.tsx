f3be7029e9d56c7fbb8dd3f0880a4ecb7fb9800c
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-10-12 08:25:12 +0400

    test

--- Shared/Components/Client/Header/Header.tsx
+++ Shared/Components/Client/Header/Header.tsx
@@ -137,7 +137,7 @@ export default function Header() {
                  <h1>Sağlam gələcək.</h1>
           </div>
           <div className={styles.headerSmallText}>
-            <h1 className=''>Həkiminizin sağlamlığınız üçün təyin etdiyi bütün dərmanlar bizdə.</h1>
+            <h1 className=''>Həkiminizin sağlamlığınız üçün təyin etdiyi bütün dərmanlar bizdə</h1>
             <h1>biz bunu sizə çatdıra bilərik.</h1>
           </div>
         </div>
