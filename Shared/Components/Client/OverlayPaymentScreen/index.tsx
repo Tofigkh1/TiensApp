0a1c0177d0b84f218d9c14b25b72057fd2f2fb53
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-10-28 20:49:53 +0400

    test1

--- Shared/Components/Client/OverlayPaymentScreen/index.tsx
+++ Shared/Components/Client/OverlayPaymentScreen/index.tsx
@@ -1,111 +1,112 @@
-import { useRef, useState } from 'react';
-import styles from './overlayPayment.module.css'
-import Image from 'next/image';
-import closedBag from '../../../../public/left-chevron.png';
-import shoppingBag from '../../../../public/shopping-bag.png';
-import { WhatsApp } from '@mui/icons-material';
+import { useRef, useState, useEffect } from 'react';
+import styles from './overlayPayment.module.css';
 import WhatsAppButton from '../whatsappButton';
 import TelegramButton from '../telegramButton';
 import { useResize } from '../../../Hooks/useResize';
 
-
-const OverlayPayment = () =>{
+const OverlayPayment = () => {
     const menuRef = useRef<HTMLDivElement>(null);
+    const overlayRef = useRef<HTMLDivElement>(null);
     const [isMenuOpen, setIsMenuOpen] = useState(true);
-    let { isMobile } = useResize();
+    const { isMobile } = useResize();
 
     const handleToggleMenu = () => {
-        setIsMenuOpen(false);
-      };
-    
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-    return(
+    // Bileşenin dışına tıklayınca menüyü kapatmak için olay dinleyici ekle
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                menuRef.current &&
+                overlayRef.current &&
+                !menuRef.current.contains(event.target as Node) &&
+                !overlayRef.current.contains(event.target as Node)
+            ) {
+                setIsMenuOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
         <>
-   {!isMobile &&
-        <div className=' '>
+            {!isMobile && (
+                <div>
+                    {isMenuOpen && (
+                        <div
+                            className='rounded-lg w-7/12 h-80 bg-white absolute z-50'
+                            style={{
+                                left: '50%',
+                                top: '50%',
+                                transform: 'translate(-50%, -50%)',
+                            }}
+                            ref={menuRef}
+                        >
+                            <h1 className='text-center text-3xl font-semibold mt-7'>
+                                Mehsulun almaq ucun zehmet olmasa bizimle elaqeye kecerek odenis edin!
+                            </h1>
+                            <div className='flex gap-10 ml-20 mt-14'>
+                                <div>
+                                    <WhatsAppButton />
+                                </div>
+                                <div>
+                                    <TelegramButton />
+                                </div>
+                            </div>
+                        </div>
+                    )}
 
-    
-         {isMenuOpen ?  <div className=' rounded-lg w-7/12 h-80 bg-white absolute z-50' style={{
-                    left: '50%',          // Sol kenarı %50
-                    top: '50%',           // Üst kenarı %50
-                    transform: 'translate(-50%, -50%)', // Kendini %50 sola ve %50 yukarı kaydır
-                }}>
-                    <h1 className='text-center text-3xl font-semibold mt-7'> Mehsulun almaq ucun zehmet olmasa bizimle elaqeye kecerek odenis edin!</h1>
-
-                    {/* <button onClick={handleToggleMenu}>click</button> */}
-
-                    <div className=' flex gap-10 ml-20 mt-14'>
-                    <div>
-                      <WhatsAppButton/>
-                    </div>
-
-                    <div>
-                      <TelegramButton/>
-                    </div>
-                    </div>
-                 
-                </div>:''}
-      
-
-
-        <div className={`${styles.overlay} ${isMenuOpen ? styles.overlayVisible : styles.overlayHidden}`}></div>
-  
-        <div className={`${styles.menu} ${isMenuOpen ? styles.menuOpen : styles.menuClosed}`}></div>
-          <div ref={menuRef} className={`${styles.menu} ${isMenuOpen ? styles.menuOpen : styles.menuClosed}`}></div>
-       
-        
-              
-  
-
+                    {isMenuOpen && (
+                        <div
+                            ref={overlayRef}
+                            className={`${styles.overlay} ${isMenuOpen ? styles.overlayVisible : styles.overlayHidden}`}
+                            onClick={() => setIsMenuOpen(false)}
+                        ></div>
+                    )}
                 </div>
-}
+            )}
 
+            {isMobile && (
+                <div>
+                    {isMenuOpen && (
+                        <div
+                            className={styles.overlayContainer}
+                            style={{
+                                left: '50%',
+                                top: '50%',
+                                transform: 'translate(-50%, -50%)',
+                            }}
+                            ref={menuRef}
+                        >
+                            <h1 className='text-center text-3xl font-semibold mt-7'>
+                                Mehsulun almaq ucun zehmet olmasa bizimle elaqeye kecerek odenis edin!
+                            </h1>
+                            <div className='flex gap-7 ml-4 mt-14'>
+                                <div>
+                                    <WhatsAppButton />
+                                </div>
+                                <div>
+                                    <TelegramButton />
+                                </div>
+                            </div>
+                        </div>
+                    )}
 
-{isMobile &&
+                    {isMenuOpen && (
+                        <div
+                            ref={overlayRef}
+                            className={`${styles.overlay} ${isMenuOpen ? styles.overlayVisible : styles.overlayHidden}`}
+                            onClick={() => setIsMenuOpen(false)}
+                        ></div>
+                    )}
+                </div>
+            )}
+        </>
+    );
+};
 
-<div className=' '>
-
-    
-{isMenuOpen ?  <div className=' rounded-lg w-11/12 h-96 bg-white absolute z-50' style={{
-           left: '50%',          // Sol kenarı %50
-           top: '50%',           // Üst kenarı %50
-           transform: 'translate(-50%, -50%)', // Kendini %50 sola ve %50 yukarı kaydır
-       }}>
-           <h1 className='text-center text-3xl font-semibold mt-7'> Mehsulun almaq ucun zehmet olmasa bizimle elaqeye kecerek odenis edin!</h1>
-
-           {/* <button onClick={handleToggleMenu}>click</button> */}
-
-           <div className=' flex gap-7 ml-4 mt-14'>
-           <div>
-             <WhatsAppButton/>
-           </div>
-
-           <div>
-             <TelegramButton/>
-           </div>
-           </div>
-        
-       </div>:''}
-
-
-
-<div className={`${styles.overlay} ${isMenuOpen ? styles.overlayVisible : styles.overlayHidden}`}></div>
-
-<div className={`${styles.menu} ${isMenuOpen ? styles.menuOpen : styles.menuClosed}`}></div>
- <div ref={menuRef} className={`${styles.menu} ${isMenuOpen ? styles.menuOpen : styles.menuClosed}`}></div>
-
-
-     
-
-
-       </div>
-}
-
-
-
-      </>
-    )
-
-}
-
-export default OverlayPayment
+export default OverlayPayment;
