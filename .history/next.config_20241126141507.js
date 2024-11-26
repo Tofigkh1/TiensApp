00e1fb5e45b32e2008068c40744214055128846f
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-11-26 14:23:27 +0400

    test

--- .history/next.config_20241126141507.js
+++ .history/next.config_20241126141507.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['firebasestorage.googleapis.com', 'images.pexels.com','tiens.com.tr', 'img.freepik.com', 'obsjtmm.jtmm.com','encrypted-tbn0.gstatic.com',"w0.peakpx.com","www.tianshi.az","www.tiens-so.com","strgimgr.umico.az",'m.media-amazon.com','tiensusa.com','ir-i.tiens.com','www.tienspartner.com','down-id.img.susercontent.com','tiensperufortuna.com','cdn.dsmcdn.com','gumrukdeposu.net'],
+    domains: ['firebasestorage.googleapis.com', 'images.pexels.com','tiens.com.tr', 'img.freepik.com', 'obsjtmm.jtmm.com','encrypted-tbn0.gstatic.com',"w0.peakpx.com","www.tianshi.az","www.tiens-so.com","strgimgr.umico.az",'m.media-amazon.com','tiensusa.com','ir-i.tiens.com','www.tienspartner.com','down-id.img.susercontent.com','tiensperufortuna.com','cdn.dsmcdn.com'],
     formats: ['image/avif', 'image/webp'],
   },
 };
